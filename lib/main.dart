2330814bc1b4b07bc39b660032bc5d55be4b7a78
Suku <ilzammulkhaq85@gmail.com>
2020-01-21 22:24:12 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:adaptive_library/adaptive_library.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveInheritance(
+      adaptiveState: AdaptiveInheritance
+          .getStateByPlatform(), // AdaptiveInheritance.getStateByPlatform(),
+      child: AdaptiveApp(
+        materialTheme: ThemeData(
+            accentColor: Colors.red,
+            buttonTheme: ButtonThemeData(
+              buttonColor: Colors.red,
+            )),
+        home: HomeScreenPage(),
+      ),
+    );
+  }
+}
+
+class HomeScreenPage extends StatefulWidget {
+  @override
+  _HomeScreenPageState createState() => _HomeScreenPageState();
+}
+
+class _HomeScreenPageState extends State<HomeScreenPage> {
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AdaptiveScaffold(
+      scaffoldKey: _globalKey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            AdaptiveProgressIndicator(),
+            AdaptiveListTile(
+              title: Text('Brightness'),
+              subtitle: Text('Change brightness'),
+              onTap: () {
+                AdaptiveAlertDialog.show<String>(
+                  context,
+                  adaptiveState: AdaptiveState
+                      .Cupertino, //AdaptiveInheritance.getStateByPlatform(),
+                  title: Text('Brightness cannot be set'),
+                  content: Text('Truly, this is only an example application.'),
+                  actions: [
+                    AdaptiveAlertDialogButton(
+                      child: Text("I'll try my phone app"),
+                      onPressed: null,
+                    ),
+                  ],
+                );
+              },
+            ),
+            AdaptiveIconButton(
+              icon: Icon(Icons.check_circle_outline),
+              iconCupertino: Icon(CupertinoIcons.check_mark_circled),
+              onPressed: () async {
+                String res = await AdaptiveAlertDialog.show<String>(
+                  context,
+                  adaptiveState: AdaptiveState
+                      .Cupertino, //AdaptiveInheritance.getStateByPlatform(),
+                  title: Text('Just saying hello'),
+                  content: Text('Your content in a dialog goes here.'),
+                  actions: [
+                    AdaptiveAlertDialogButton(
+                      child: Text('OK'),
+                      closeOnPress: false,
+                      destructive: true,
+                      onPressed: () {
+                        //do stuff here
+                        //Navigator.pop(context, "ok-press");
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1900),
+                          lastDate: DateTime(2100),
+                        );
+                      },
+                    ),
+                    AdaptiveAlertDialogButton(
+                      closeOnPress: true,
+                      child: Text('Hello!'),
+                      onPressed: null,
+                    ),
+                    AdaptiveAlertDialogButton(
+                      child: Text('Thanks'),
+                      onPressed: null,
+                    ),
+                  ],
+                );
+
+                print('result: $res');
+
+                if (res == null) return;
+
+                _globalKey.currentState.showSnackBar(SnackBar(
+                  content: Text('You just pressed ok, I think'),
+                  action: SnackBarAction(
+                    label: 'Yes',
+                    onPressed: () => null,
+                  ),
+                ));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
